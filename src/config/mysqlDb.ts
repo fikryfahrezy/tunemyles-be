7ea37421dd82b8349fa9ca2cf1dc6e590c63bc66
Fikry Fahrezy Ramadhan <ffahrezi95@yahoo.com>
2021-02-15 21:18:39 +0700

    re-config logger, and mysql connection

--- src/config/mysqlDb.ts
+++ src/config/mysqlDb.ts
@@ -1,30 +1,26 @@
-// 'use strict';
-
 import sq from "sequelize";
 import cls from "cls-hooked";
+import logger from "../utils/logger";
+
 const namespace = cls.createNamespace("namespace");
-// import logger from'./utils/logger'
-const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env as {
+  [k: string]: string;
+};
 
 sq.Sequelize.useCLS(namespace);
 
-const sequelize = new sq.Sequelize(DB_NAME!, DB_USERNAME!, DB_PASSWORD, {
+export const sequelize = new sq.Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
-  port: Number(DB_PORT)!,
+  port: Number(DB_PORT),
   dialect: "mysql",
+  logging: (msg) => logger.debug(msg),
 });
 
-async function sequelizeConnect() {
+export async function sequelizeConnect(): Promise<void> {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    logger.info("Connection has been established successfully.");
   } catch (error) {
-    console.log("Unable to connect to the database:", error);
+    logger.error("Unable to connect to the database: %s", error);
   }
 }
-
-module.exports = {
-  sq,
-  sequelize,
-  sequelizeConnect,
-};
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 require("make-promises-safe");
 import dotenv from "dotenv";
-import app from "./app";
-
+import pino from "pino";
 dotenv.config();
 
+import app from "./app";
+import { sequelizeConnect } from "./config/mysqlDb";
+import logger, { dest } from "./utils/logger";
+
 const PORT = process.env.PORT;
-const server = app({ logger: true });
+const server = app({ logger });
 
 const start = async () => {
   try {
+    await sequelizeConnect();
     await server.listen({ port: Number(PORT), host: "::" });
     const address = server.server.address();
     const port = typeof address === "string" ? address : address?.port;
@@ -19,3 +23,27 @@ const start = async () => {
   }
 };
 start();
+
+// asynchronously flush every 10 seconds to keep the buffer empty
+// in periods of low activity
+setInterval(function () {
+  logger.flush();
+}, 10000).unref();
+
+// use pino.final to create a special logger that
+// guarantees final tick writes
+const handler = pino.final(logger, (err, finalLogger, evt) => {
+  finalLogger.info(`${evt} caught`);
+  if (err) finalLogger.error(err, "error caused exit");
+  process.exit(err ? 1 : 0);
+});
+
+process.on("SIGHUP", () => dest.reopen());
+
+// catch all the ways node might exit
+process.on("beforeExit", () => handler(null, "beforeExit"));
+process.on("exit", () => handler(null, "exit"));
+process.on("uncaughtException", (err) => handler(err, "uncaughtException"));
+process.on("SIGINT", () => handler(null, "SIGINT"));
+process.on("SIGQUIT", () => handler(null, "SIGQUIT"));
+process.on("SIGTERM", () => handler(null, "SIGTERM"));
